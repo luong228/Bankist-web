bdd221d4f69147a6f2cfe0d0954765a7963d19df
luong228 <90311663+luong228@users.noreply.github.com>
2023-11-17 11:14:23 +0700

    Building tabled content

--- script.js
+++ script.js
@@ -184,4 +184,23 @@ console.log(h1.closest('h1'));
 // Queryselector Find children no matter how far - Closest find parent
 
 
-// Table Component
+//Tabled component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabContainer.addEventListener('click', (e) => {
+  const clicked = e.target.closest('.operations__tab')
+
+  //Guard clause
+  if(!clicked) return;
+
+  //Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'))
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'))
+
+  
+  clicked.classList.add('operations__tab--active')
+  //Activate Content area
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
+})
