947f908346d1ac70d932a2b4bedf6dde77633f79
luong228 <90311663+luong228@users.noreply.github.com>
2023-12-05 09:57:44 +0700

    Revealing Element on Scroll

--- script.js
+++ script.js
@@ -250,6 +250,7 @@ const navHeight = nav.getBoundingClientRect().height
 
 const stickyNav = function(entries) {
   const [entry] = entries; // Equal to entry = entries[0]
+  // console.log(entry);
   if(!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
 
@@ -262,3 +263,24 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 })
   
 headerObserver.observe(header)
+
+//Reveal section when approaching
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if(!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target); // Remove event listening as we did all work we wanted
+}
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null, // viewport
+  threshold: 0.15,
+})
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden')
+})
