ad03f10d4cc4b0990ef4d71198d1f3e6484f4f29
luong228 <90311663+luong228@users.noreply.github.com>
2023-12-06 14:20:37 +0700

    Content loading

--- script.js
+++ script.js
@@ -378,3 +378,16 @@ const slider = () => {
 };
 
 slider();
+
+document.addEventListener('DOMContentLoaded', e => {
+  console.log('HTML parsed and DOM tree built', e);
+});
+
+window.addEventListener('load', e => {
+  console.log('Page fully loaded', e);
+});
+
+window.addEventListener('beforeunload', e => {
+  e.preventDefault();
+  e.returnValue = true;
+});
